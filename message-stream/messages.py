cb5d4b854fc789a5ba5f6d88cad6044bacdb37d2
numa08 <937552+numa08@users.noreply.github.com>
2022-07-14 23:01:18 +0900

    画像を表示する
    pillowを入れる必要がある

--- message-stream/messages.py
+++ message-stream/messages.py
@@ -1,6 +1,9 @@
 import os
 import firebase_admin
 from firebase_admin import credentials, firestore
+import urllib.request
+import tempfile
+from PIL import Image
 
 # Use a service account
 cred = credentials.Certificate(
@@ -17,6 +20,15 @@ def on_snapshot(collection_snapshot, changes, read_time):
     os.system('clear')
     for doc in collection_snapshot:
         print(f'\033[1m{doc.get("name")}\033[0m:\n{doc.get("message")}\n')
+        image = doc.get('image')
+        if image is not None:
+            with tempfile.TemporaryDirectory() as tmpdir:
+                image_file = os.path.join(tmpdir, 'image.jpg')
+                urllib.request.urlretrieve(image, image_file)
+                img = Image.open(image_file)
+                img.thumbnail((1024, 1024))
+                img.save(image_file)
+                os.system(f'imgcat {image_file}')
 
 
 message_ref.on_snapshot(on_snapshot)
